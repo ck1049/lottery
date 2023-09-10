9ed973cfa96603efd0c452258c95fa7069406801
ck1049 <18238489029@163.com>
2023-09-10 19:53:41 +0800

    继承自spring-boot-starter-parent

--- src/main/java/com/reward/lottery/LotteryApplication.java
+++ src/main/java/com/reward/lottery/LotteryApplication.java
@@ -2,6 +2,9 @@ package com.reward.lottery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
@@ -10,6 +13,11 @@ public class LotteryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LotteryApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 
 }
